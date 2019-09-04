a4bc3b8c62e48e5a49dd217a248ffdd3c8e2c6ec
raineer <delaritaraineer81@gmail.com>
2019-09-04 16:34:38 +0800

    cors credentials

--- app.js
+++ app.js
@@ -48,19 +48,30 @@ app.use(
   })
 );
 
-app.use((req, res, next) => {
+// app.use((req, res, next) => {
+//   res.header("Access-Control-Allow-Origin", "*");
+//   res.header(
+//     "Access-Control-Allow-Headers",
+//     "Origin, X-Requested-With, Content-Type,Accept, Authorization"
+//   );
+//   if (req.method === "OPTIONS") {
+//     res.header(
+//       "Access-Control-Allow-Methods",
+//       "PUT, POST, ,PATCH, DELETE, GET"
+//     );
+//     return res.status(200).json({});
+//   }
+//   next();
+// });
+
+app.use(function(req, res, next) {
   res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Credentials", true);
+  res.header("Access-Control-Allow-Methods", "GET,PUT,POST,DELETE,OPTIONS");
   res.header(
     "Access-Control-Allow-Headers",
-    "Origin, X-Requested-With, Content-Type,Accept, Authorization"
+    "Origin,X-Requested-With,Content-Type,Accept,content-type,application/json"
   );
-  if (req.method === "OPTIONS") {
-    res.header(
-      "Access-Control-Allow-Methods",
-      "PUT, POST, ,PATCH, DELETE, GET"
-    );
-    return res.status(200).json({});
-  }
   next();
 });
 
