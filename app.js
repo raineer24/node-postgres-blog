9c91b0d7aa43126a818ee678592cab6126d9a261
raineer <delaritaraineer81@gmail.com>
2019-09-04 16:29:59 +0800

    acces control

--- app.js
+++ app.js
@@ -48,21 +48,21 @@ app.use(
   })
 );
 
-// app.use((req, res, next) => {
-//   res.header("Access-Control-Allow-Origin", "*");
-//   res.header(
-//     "Access-Control-Allow-Headers",
-//     "Origin, X-Requested-With, Content-Type,Accept, Authorization"
-//   );
-//   if (req.method === "OPTIONS") {
-//     res.header(
-//       "Access-Control-Allow-Methods",
-//       "PUT, POST, ,PATCH, DELETE, GET"
-//     );
-//     return res.status(200).json({});
-//   }
-//   next();
-// });
+app.use((req, res, next) => {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type,Accept, Authorization"
+  );
+  if (req.method === "OPTIONS") {
+    res.header(
+      "Access-Control-Allow-Methods",
+      "PUT, POST, ,PATCH, DELETE, GET"
+    );
+    return res.status(200).json({});
+  }
+  next();
+});
 
 app.use("/api/v2/blog", blogRoute);
 app.use("/api/v2/useraccount", useraccountRoute);
